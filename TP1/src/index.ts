58028740a2e4f1b187e4f1a1ab366fae232a0a6a
Mateo Muscolino <muscolinomateo@gmail.com>
2025-08-29 16:44:49 -0300

    ej4 tp1

--- TP1/src/index.ts
+++ TP1/src/index.ts
@@ -1,8 +1,10 @@
 import { ejecutarInciso1 } from "./incisoUno";
 import { ejecutarInciso2 } from "./incisoDos";
 import { ejecutarInciso3 } from "./incisoTres";
+import { ejecutarInciso4 } from "./incisoCuatro";
 
 console.log("-------------------tp1----------------------");
 ejecutarInciso1();
 ejecutarInciso2();
 ejecutarInciso3();
+ejecutarInciso4();
