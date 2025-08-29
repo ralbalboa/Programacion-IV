279318fa504252fc077c1ea09fb5542d44896756
Mateo Muscolino <muscolinomateo@gmail.com>
2025-08-29 09:38:18 -0300

    ej2 tp1

--- TP1/src/index.ts
+++ TP1/src/index.ts
@@ -1,4 +1,7 @@
 import { ejecutarInciso1 } from "./incisoUno";
+import { ejecutarInciso2 } from "./incisoDos";
+
 
 console.log("-------------------tp1----------------------");
 ejecutarInciso1();
+ejecutarInciso2();
