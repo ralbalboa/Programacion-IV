e20370e9ba5868008ab92cef64b76f0e54ffcc07
Mateo Muscolino <muscolinomateo@gmail.com>
2025-10-05 14:45:35 -0300

    OrderService para la coordinacion y almacenamiento de las ordenes

--- TP2/src/services/orderService.ts
+++ TP2/src/services/orderService.ts
@@ -1,16 +1,20 @@
-// import { v4 as uuidv4 } from 'uuid';
-// import { orderSize, Order } from '../types/orderTypes';
+import {Order} from "../models/Order";
+import { CreateOrder } from "../types/orderTypes";
 
- 
-// export class OrderService{
-//     private static Orders:Order[] = [];
-    
-//     static createOrder(orderData:{toppings:string[],orderSize:orderSize, address:string, orderStatus:orderStatus}){
-//         const newOrder = {
-//             id: uuidv4(),
-//             toppings: orderData.toppings,
-//             orderSize: orderData.orderSize,
-//             address: orderData.address
-//         }
-//     }
-// }
+let orders: Order[] = []; 
+
+export class OrderService {
+    // crear orden
+    public createOrder(orderData: CreateOrder): Order {
+        const newOrder = new Order(orderData.orderSize, orderData.toppings, orderData.address);
+        orders.push(newOrder);
+        return newOrder;
+    }
+    //buscar por id
+
+    //cancelar orden
+
+    //listar
+
+    //listar por estado
+}
