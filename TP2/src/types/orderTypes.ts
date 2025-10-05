8045c87ec9f73800bf8e58bb325cc594acd1da01
Mateo Muscolino <muscolinomateo@gmail.com>
2025-10-05 14:23:29 -0300

    OrderSchema con zod

--- TP2/src/types/orderTypes.ts
+++ TP2/src/types/orderTypes.ts
@@ -1,11 +1,23 @@
-export type orderSize = "s" | "m" | "l"
-export type orderStatus = "delivered" | "pending"
+import {z} from 'zod';
 
-export type Order = {
-    id:string;
-    orderSize: orderSize;
-    orderStatus: orderStatus;
-    toppings: string[];
-    address:string;
-}
+const orderSizeSchema = z.enum(['S', 'M', 'L']);
 
+
+export const createOrderSchema = z.object({
+    orderSize: orderSizeSchema,
+
+    toppings: 
+    z
+    .array(z.string())
+    .max(5,{message: "No puede tener más de 5 toppings"})
+    .min(1, {message: "Debe tener al menos un topping"}),
+
+    address: 
+    z.
+    string()
+    .min(10,{
+        message: "La dirección debe tener al menos 10 caracteres"
+    })
+})
+
+export type CreateOrder = z.infer<typeof createOrderSchema>; // significa que cualquier dato que pase la validación Zod sera del tipo CreateOrder
